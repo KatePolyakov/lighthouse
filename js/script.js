95548ea28f7681bcce6d522876ab84ca245c05f0
Kate <polyakovkate@gmail.com>
2023-12-21 20:55:27 -0800

    lighthouse done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+// $('.parallax_list>li').addClass('layer');
+$('.parallax_list').parallax();
+
+  $('.wrapper').addClass('active');
+
+
+
+  
+
+
+
+
+
+
+
+
+});
+
